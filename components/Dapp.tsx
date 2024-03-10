6c13c991310d77cf7caf5b50f3d35cc07791fab8
michaelloupa <michaelloupa@gmail.com>
2024-03-10 18:07:40 +0300

    updated the dappstore and the dapp page

--- components/Dapp.tsx
+++ components/Dapp.tsx
@@ -6,14 +6,14 @@ type Props = {}
 
 const Dapp = (props: Props) => {
     return (
-        <Link href={'https://www.halofi.me/'} className="p-5  ">
+        <Link href={'/dappstore/dapp'} className="p-5  ">
             <div className="p-5 flex gap-2 justify-center border-b border-solid border-neutral-400">
                 <Image src={Halofi} alt="halofi" className="shrink-0 my-auto aspect-[1.43] w-[90px]"
                     />
                 <div className="flex flex-col grow shrink-0 justify-center px-5 mt-1.5 basis-0 w-fit">
                     <div className="flex ">
                         <div className="flex-auto text-xs font-extrabold tracking-wider leading-5 text-fuchsia-700 uppercase">
-                    Halofi
+                    Swypt
                         </div>
                         <div className="justify-center px-1.5 my-auto text-xs font-semibold leading-4 text-white whitespace-nowrap rounded-md bg-blue-950">
                     ad
