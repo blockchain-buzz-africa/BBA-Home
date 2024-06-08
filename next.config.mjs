68c29cb8c09695f2e47f8af7f84303d46005c053
michaelloupa <michaelloupa@gmail.com>
2024-06-08 20:59:53 +0300

    made changes to banner

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,7 @@
 const nextConfig = {
     images: {
         domains: ["res.cloudinary.com"]
-    },
-    staticPageGenerationTimeout: 120, // Increase timeout to 120 seconds
+    }
 };
 
 export default nextConfig;
